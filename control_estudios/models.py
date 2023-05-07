954a6456078a8c978d76fe4164b53b3b7d1d2914
mpazdonicelli <pachudonicelli@hotmail.com>
2023-05-07 11:19:41 -0300

    Agregado archivo readme

--- control_estudios/models.py
+++ control_estudios/models.py
@@ -13,10 +13,10 @@ class Curso(models.Model):
 class Estudiante(models.Model):
     apellido = models.CharField(max_length=256)
     nombre = models.CharField(max_length=256)
-    email = models.EmailField()
+    email = models.EmailField(blank=True)
     telefono = models.CharField(max_length=20, blank=True)
     dni = models.CharField(max_length=32)
-    fecha_nacimiento = models.DateField()
+    fecha_nacimiento = models.DateField(null=True)
 
     def __str__(self):
         return f"{self.apellido}, {self.nombre}"
@@ -30,6 +30,8 @@ class Profesor(models.Model):
     profesion = models.CharField(max_length=128)
     bio = models.TextField()
 
+    def __str__(self):
+        return f"{self.apellido}, {self.nombre}"
 
 class Entregable(models.Model):
     nombre = models.CharField(max_length=256)
